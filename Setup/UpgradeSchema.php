b088b1542091b2b14f6dedcef44f1cd1c5adb265
dixonpang <dixon.pang@cleargo.com>
2018-01-17 18:30:22 +0800

    Add Order attribute nav_last_sync_at with upgrade schema

--- Setup/UpgradeSchema.php
+++ Setup/UpgradeSchema.php
@@ -21,6 +21,21 @@ class UpgradeSchema implements UpgradeSchemaInterface
         if (version_compare($context->getVersion(), "1.0.0", "<")) {
         //Your upgrade script
         }
+
+        if (version_compare($context->getVersion(), "1.0.1", "<")) {
+            //Your upgrade script
+            $orderTable = 'sales_order';
+            //Order table
+            $setup->getConnection()
+                ->addColumn(
+                    $setup->getTable($orderTable),
+                    'nav_last_sync_at',
+                    [
+                        'type' => \Magento\Framework\DB\Ddl\Table::TYPE_DATETIME,
+                        'comment' =>'Nav Last Sync At'
+                    ]
+                );
+        }
         $setup->endSetup();
     }
 }
