d685ba0a48d88062b96340dbe8b3b8da8871f22a
dixonpang <dixon.pang@cleargo.com>
2018-01-22 16:44:49 +0800

    Add Custom log for Nav

---
+++ Logger/Handler.php
@@ -0,0 +1,19 @@
+<?php
+namespace Cleargo\Integrationframeworks\Logger;
+
+use Monolog\Logger as BaseLogger;
+
+class Handler extends \Magento\Framework\Logger\Handler\Base
+{
+    /**
+     * Logging level
+     * @var int
+     */
+    protected $loggerType = BaseLogger::INFO;
+
+    /**
+     * File name
+     * @var string
+     */
+    protected $fileName = '/var/log/nav/general.log';
+}
---
+++ Logger/Logger.php
@@ -0,0 +1,9 @@
+<?php
+namespace Cleargo\Integrationframeworks\Logger;
+
+use Monolog\Logger as BaseLogger;
+
+class Logger extends BaseLogger
+{
+
+}
