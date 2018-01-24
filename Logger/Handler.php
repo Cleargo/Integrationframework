1eadc07b592594674efb6ba11f6c5963e0aaf139
dixonpang <dixon.pang@cleargo.com>
2018-01-24 10:47:59 +0800

    Chage log folder path to integrationframeworks

--- Logger/Handler.php
+++ Logger/Handler.php
@@ -15,5 +15,5 @@ class Handler extends \Magento\Framework\Logger\Handler\Base
      * File name
      * @var string
      */
-    protected $fileName = '/var/log/nav/general.log';
+    protected $fileName = '/var/log/integrationframeworks/general.log';
 }
