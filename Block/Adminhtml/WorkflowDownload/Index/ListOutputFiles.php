0e9b1ee967a88a2ad9935fbf963054b0e386b68c
PY Yick <py.yick@cleargo.com>
2018-03-27 15:44:08 +0800

    Create download CSV page, list files first, download later

---
+++ Block/Adminhtml/WorkflowDownload/Index/ListOutputFiles.php
@@ -0,0 +1,94 @@
+<?php
+/**
+ * List1
+ *
+ * @copyright Copyright © 2018 PY Yick. All rights reserved.
+ * @author    py.yick@cleargo.com
+ */
+
+namespace Cleargo\Integrationframeworks\Block\Adminhtml\WorkflowDownload\Index;
+
+class ListOutputFiles extends \Magento\Framework\View\Element\Template
+{
+    const SCHEDULE_ID = 5;
+    
+    protected $directoryList;
+
+    protected $workflowScheduleFactory;
+
+    /**
+     * @var \Magento\Framework\View\Result\PageFactory
+     */
+    protected $resultPageFactory;
+
+    public function __construct(
+        \Magento\Framework\View\Element\Template\Context $context,
+        \Magento\Framework\Filesystem\DirectoryList $directoryList,
+        \Cleargo\Integrationframeworks\Model\WorkflowScheduleFactory $workflowScheduleFactory,
+        array $data
+    )
+    {
+        parent::__construct($context, $data);
+        $this->directoryList = $directoryList;
+        $this->workflowScheduleFactory = $workflowScheduleFactory;
+    }
+
+    public function execute()
+    {
+        return  $resultPage = $this->resultPageFactory->create();
+    }
+    
+    public function getFileList(){
+        $var_location = $this->directoryList->getRoot();
+        //Load workflow directory settings
+        $workflow = $this->workflowScheduleFactory->create()->load(self::SCHEDULE_ID);
+        $workflow->loadRelation();
+        $relations = $workflow->getRelation();
+        foreach ($relations as $rel) {
+            $params = $rel['parameters'];
+            $params = json_decode($params, true);
+            $path_set = $var_location . $params['export_path'];
+            $folder_content = scandir($path_set);
+            $filelist = [];
+            foreach ($folder_content as $content){
+                //Only accept CSV file
+                $file_path = $path_set . $content;
+                $fileinfo = pathinfo($file_path);
+                if (isset($fileinfo['extension']) && $fileinfo['extension'] == 'csv'){
+                    $filelist[] = $content;
+                }
+            }
+            return $filelist;
+        }
+    }
+    
+    public function processFilelist(){
+        $result = [];
+        $filelist = $this->getFileList();
+        foreach ($filelist as $file){
+            $filename_parts = explode("_", $file);
+            $idx = 'paid';
+            if ($filename_parts[3] == 'unpaid')
+                $idx = 'unpaid';
+            if (!isset($result[$filename_parts[2]][$idx]))
+                $result[$filename_parts[2]][$idx] = [];
+            array_push($result[$filename_parts[2]][$idx], $file);
+        }
+        return $result;
+    }
+
+    public function getStoreName($id){
+        $website_id = $this->_storeManager->getStore($id)->getWebsiteId();
+        return $this->_storeManager->getWebsite($website_id)->getName();
+    }
+
+    public function getFileLabel($filename){
+        $filename_parts = explode("_", $filename);
+        $idx = 3;
+        if ($filename_parts[3] == 'unpaid')
+            $idx += 1;
+        $date = $filename_parts[$idx];
+        $time = str_replace('.csv', '', $filename_parts[$idx + 1]);
+        return __("Order Export on ") . date("Y-m-d", strtotime($date)) . " " . date("H:i:s", strtotime($time));
+    }
+}
---
+++ Controller/Adminhtml/WorkflowDownload.php
@@ -0,0 +1,36 @@
+<?php
+
+
+namespace Cleargo\Integrationframeworks\Controller\Adminhtml;
+
+abstract class WorkflowDownload extends \Magento\Backend\App\Action
+{
+
+    protected $_coreRegistry;
+    const ADMIN_RESOURCE = 'Cleargo_Integrationframeworks::top_level';
+
+    /**
+     * @param \Magento\Backend\App\Action\Context $context
+     * @param \Magento\Framework\Registry $coreRegistry
+     */
+    public function __construct(
+        \Magento\Backend\App\Action\Context $context,
+        \Magento\Framework\Registry $coreRegistry
+    ) {
+        $this->_coreRegistry = $coreRegistry;
+        parent::__construct($context);
+    }
+
+    /**
+     * Init page
+     *
+     * @param \Magento\Backend\Model\View\Result\Page $resultPage
+     */
+    public function initPage($resultPage)
+    {
+        $resultPage->setActiveMenu(self::ADMIN_RESOURCE)
+            ->addBreadcrumb(__('Cleargo'), __('Cleargo'))
+            ->addBreadcrumb(__('Download Output Files'), __('Download Output Files'));
+        return $resultPage;
+    }
+}
