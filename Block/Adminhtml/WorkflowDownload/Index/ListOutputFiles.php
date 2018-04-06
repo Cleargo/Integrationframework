eed354d512130a70dee684739b7fd51fcfcfad65
PY Yick <py.yick@cleargo.com>
2018-04-06 13:07:29 +0800

    Finalize the order export for SAP Add new system variable for delivery date Add new field mapping for different brands Modify the method of list output files

--- Block/Adminhtml/WorkflowDownload/Index/ListOutputFiles.php
+++ Block/Adminhtml/WorkflowDownload/Index/ListOutputFiles.php
@@ -37,42 +37,15 @@ class ListOutputFiles extends \Magento\Framework\View\Element\Template
     {
         return  $resultPage = $this->resultPageFactory->create();
     }
-    
-    public function getFileList(){
-        $var_location = $this->directoryList->getRoot();
-        //Load workflow directory settings
-        $workflow = $this->workflowScheduleFactory->create()->load(self::SCHEDULE_ID);
-        $workflow->loadRelation();
-        $relations = $workflow->getRelation();
-        foreach ($relations as $rel) {
-            $params = $rel['parameters'];
-            $params = json_decode($params, true);
-            $path_set = $var_location . $params['export_path'];
-            $folder_content = scandir($path_set);
-            $filelist = [];
-            foreach ($folder_content as $content){
-                //Only accept CSV file
-                $file_path = $path_set . $content;
-                $fileinfo = pathinfo($file_path);
-                if (isset($fileinfo['extension']) && $fileinfo['extension'] == 'csv'){
-                    $filelist[] = $content;
-                }
-            }
-            return $filelist;
-        }
-    }
-    
+
     public function processFilelist(){
         $result = [];
-        $filelist = $this->getFileList();
-        foreach ($filelist as $file){
-            $filename_parts = explode("_", $file);
-            $idx = 'paid';
-            if ($filename_parts[3] == 'unpaid')
-                $idx = 'unpaid';
-            if (!isset($result[$filename_parts[2]][$idx]))
-                $result[$filename_parts[2]][$idx] = [];
-            array_push($result[$filename_parts[2]][$idx], $file);
+        $store_list = $this->_storeManager->getGroups();
+        foreach ($store_list as $store){
+            $result[$store->getId()] = [
+                "paid",
+                "unpaid"
+            ];
         }
         return $result;
     }
@@ -82,24 +55,14 @@ class ListOutputFiles extends \Magento\Framework\View\Element\Template
         return $this->_storeManager->getWebsite($website_id)->getName();
     }
 
-    public function getFileLabel($filename){
-        $filename_parts = explode("_", $filename);
-        $idx = 3;
-        if ($filename_parts[3] == 'unpaid')
-            $idx += 1;
-        $date = $filename_parts[$idx];
-        $time = str_replace('.csv', '', $filename_parts[$idx + 1]);
-        return __("Order Export on ") . date("Y-m-d", strtotime($date)) . " " . date("H:i:s", strtotime($time));
-    }
-
-    public function getDownloadLink(){
-        return $this->getUrl('cleargo_integrationframeworks/workflowdownload/download');
-    }
-
     public function getStartEndDate(){
         return [
             'start_date' => $this->_scopeConfig->getValue('product/event/latest_event_start_date'),
             'end_date' => $this->_scopeConfig->getValue('product/event/latest_event_end_date'),
         ];
+    }
+
+    public function getDownloadLink(){
+        return $this->getUrl('cleargo_integrationframeworks/workflowdownload/download');
     }
 }
--- Controller/Adminhtml/WorkflowDownload/Download.php
+++ Controller/Adminhtml/WorkflowDownload/Download.php
@@ -69,6 +69,7 @@ class Download extends \Magento\Backend\App\Action
         $post_format = $this->getRequest()->getParam('format');
         //Separate case
         $criteria = $this->typeHandler($post_type);
+        $this->logger->info(json_encode($criteria));
         $orders = $this->getOrderCollection($criteria['website_id'], $criteria['status']);
         $file_content = $this->processOrderCollection($orders, $post_format);
         $this->fileFactory->create(
@@ -100,6 +101,8 @@ class Download extends \Magento\Backend\App\Action
         $data = $orderCollection;
         $cond_arr = [];
         $field_arr = [];
+        $this->logger->info($orderStatus);
+        $this->logger->info($store_id);
         if ($orderStatus !== null){
             $field_arr[] = 'status';
             $cond_arr[] = ['in' => $orderStatus];
@@ -119,9 +122,9 @@ class Download extends \Magento\Backend\App\Action
             $this->logger->info("ExportOrder: There are no Order for export");
             return;
         } else {
-            // Export Order Xml
             $csv_output = "";
             if ($type == 'csv') {
+                // Export Order CSV
                 $csv_header = [
                     "Sales Period",
                     "Order Date",
@@ -143,8 +146,6 @@ class Download extends \Magento\Backend\App\Action
                 ];
                 $csv_output .= $this->outputCSV($csv_header);
                 foreach ($orderCollection as $order) {
-                    $this->logger->info($order->getIncrementId());
-                    $this->logger->info($order->getCustomerId());
                     $customer_id = $order->getCustomerId();
                     //Customer
                     $loreal_entity_id = '';
@@ -189,7 +190,8 @@ class Download extends \Magento\Backend\App\Action
                     }
                 }
             } else {
-                $csv_header = [
+                // Export Order TXT
+                $csv_header[] = [
                     "",
                     "Div",
                     "Type",
@@ -211,54 +213,64 @@ class Download extends \Magento\Backend\App\Action
                     "GLN",
                     "EDISupp"
                 ];
-                $csv_output .= $this->outputCSV($csv_header, $type);
-                return $csv_output;
+                $csv_record = $csv_header;
+                $row = [];
                 foreach ($orderCollection as $order) {
-                    $this->logger->info($order->getIncrementId());
-                    $this->logger->info($order->getCustomerId());
-                    $customer_id = $order->getCustomerId();
-                    //Customer
-                    $loreal_entity_id = '';
-                    $staff_no = '';
-                    $name = '';
-                    if ($customer_id != '') {
-                        $customer = $this->customer->getById($customer_id);
-                        if ($customer->getCustomAttribute('loreal_entity_id'))
-                            $loreal_entity_id = $customer->getCustomAttribute('loreal_entity_id')->getValue();//Entity Code
-                        if ($customer->getCustomAttribute('staff_no'))
-                            $staff_no = $customer->getCustomAttribute('staff_no')->getValue();//Staff No
-                        $name = $customer->getFirstname() . " " . $customer->getLastname();//Staff Name
-                    }
-                    //Ordered Items
                     $products = $order->getAllItems();
-                    $csv_order = [
-                        $this->scopeConfig->getValue('product/event/latest_event_name'),//Sales Period
-                        date("d-m-Y", strtotime($order->getCreatedAt())),//Order Date
-                        $order->getStoreId() == 2 ? "Free Good" : "Staff Purchase",//Order Type
-                        $order->getStatus(),//Order Status
-                        $loreal_entity_id,
-                        $staff_no,
-                        $name
-                    ];
                     foreach ($products as $product) {
                         //Get Product
                         $product_factory = $this->product->create()->load($product->getProductId());
-                        $csv_order_item = [
-                            $product->getSku(),//Item Code
-                            $this->getSelectOptionText($product_factory, 'brand_name'),//Brand Name
-                            $this->getSelectOptionText($product_factory, 'brand'),//Brand
-                            $this->getSelectOptionText($product_factory, 'division'),//Division
-                            $this->getProductCategory($product_factory),//Category
-                            $product->getPrice(),//Unit Price
-                            $product->getQtyOrdered(),//Order Qty
-                            $product->getQtyInvoiced(),//Provision Qty
-                            $product->getQtyShipped(),//Received Qty
-                            $product->getName()//Description
+                        //Map brand_code with sap_division_code
+                        $brand_code = $this->getSelectOptionText($product_factory, 'brand_name');
+                        $sap_division_code = $this->mapBrandCodeSapDivisionCode($brand_code);
+                        //See if free goods checkout or staff purchase checkout
+                        $type = $order->getStoreId() == 2 ? "YFD" : "YOR";
+                        //See if free goods checkout or staff purchase checkout
+                        $wh_type = $order->getStoreId() == 2 ? "LSFG" : "LSPO";
+                        //Staff code
+                        //Get Customer id
+                        $customer_id = $order->getCustomerId();
+                        $staff_no = '';
+                        if ($customer_id != '') {
+                            $customer = $this->customer->getById($customer_id);
+                            if ($customer->getCustomAttribute('staff_no'))
+                                $staff_no = $customer->getCustomAttribute('staff_no')->getValue();
+                        }
+                        //Order Date
+                        $order_date = $this->formatDateTxt($order->getCreatedAt());
+                        //Delivery Date
+                        $delivery_date = $this->scopeConfig->getValue("product/event/latest_event_delivery_date");
+                        $delivery_date = $this->formatDateTxt($delivery_date);
+                        //Sold To, Free Goods TBC
+                        $sold_to = $order->getStoreId() == 2 ? "TBC" : "120467";
+                        $row[] = [
+                            "", //Empty separator
+                            $sap_division_code, //sap_division_code
+                            $type, //type
+                            "H00", //Reason
+                            $wh_type, //WHType
+                            $staff_no, //PORef
+                            $order_date, //OrdDate
+                            $delivery_date, //DelDate
+                            $delivery_date, //InvDate
+                            $sold_to, //SoldTo
+                            $staff_no, //ShipTo
+                            "NA", //MSICust
+                            "", //Remark
+                            $product->getSku(), //Material
+                            "X", //MSIMaterial
+                            (int)$product->getQtyOrdered(), //OrderQty
+                            "0", //FreeQty
+                            (int)$product->getPrice(), //Price
+                            "", //GLN
+                            "", //EDISupp
                         ];
-                        $csv_record = array_merge($csv_order, $csv_order_item);
-                        $csv_output .= $this->outputCSV($csv_record);
+                        $csv_record = array_merge($csv_record, $row);
                     }
                 }
+                $this->logger->info(json_encode($csv_record));
+                foreach ($csv_record as $line)
+                    $csv_output .= $this->outputCSV($line, $type) . "\r\n";
             }
             return $csv_output;
         }
@@ -297,5 +309,58 @@ class Download extends \Magento\Backend\App\Action
             }
         }
         return $cat_name[0];
+    }
+
+    protected function mapBrandCodeSapDivisionCode($brand_code){
+        $map = [
+            "" => "",
+            "BIO" => "32",
+            "GAB" => "3A",
+            "GRN" => "17",
+            "HR" => "33",
+            "KIE" => "38",
+            "LRP" => "41",
+            "LAN" => "31",
+            "LOP" => "17",
+            "MTX" => "22",
+            "MBL" => "17",
+            "SHU" => "34",
+            "SKC" => "42",
+            "STL" => "3H",
+            "VIC" => "40",
+            "YSL" => "3E",
+            "ZEG" => "3I",
+            "GAX" => "3A",
+            "GAY" => "3A",
+            "GRX" => "17",
+            "KIX" => "38",
+            "LOZ" => "17",
+            "LOY" => "17",
+            "LOX" => "17",
+            "LOW" => "17",
+            "LOV" => "17",
+            "LPX" => "20",
+            "MBX" => "17",
+            "RLX" => "37",
+            "SLM" => "3H",
+            "LPP" => "20",
+            "HER" => "33",
+            "SUA" => "34",
+            "SKS" => "42",
+            "DSL" => "3B",
+            "KER" => "21",
+            "MMM" => "3K",
+            "VRF" => "3D",
+            "PLY" => "20",
+            "CLA" => "3L",
+            "UD" => "3P",
+            "AC" => "3Q",
+            "R&G" => "3F",
+        ];
+        return $map[$brand_code];
+    }
+
+    protected function formatDateTxt($date){
+        return date("Ymd", strtotime($date));
     }
 }
